946b0a5cde926503e34fab0b59e00b10ebd5cf69
Sofia <sofiaestanqueirorocha@gmail.com>
2019-06-17 17:43:11 +0100

    rocha_sofia_020 quest

--- front/src/components/SignUp.js
+++ front/src/components/SignUp.js
@@ -4,27 +4,61 @@ class SignUp extends Component {
     constructor(props) {
         super(props);
         this.state = {
-            email: 'new email'
+            email: '',
+            password: '',
+            firstName: '',
+            lastName: ''
         }
     }
 
     updateEmailField = (event) => {
         this.setState({
-            email: event.target.value
+            [event.target.name] : event.target.value
         });
     }
 
+    handleSubmit = (event) => {
+        event.preventDefault();
+        console.log(JSON.stringify(this.state));
+    }
+
     render() {
-        const { email } = this.state;
         return (
             <div>
-                <h1>{email}</h1>
-                <input
-                    type='email'
-                    name='email'
-                    placeholder='Enter your email...'
-                    onChange={this.updateEmailField}
-                />
+                <h1 className='title'>{JSON.stringify(this.state)}</h1>
+                <form className='form' onSubmit={this.handleSubmit}>
+                    <input
+                        type='email'
+                        name='email'
+                        placeholder='Enter your email...'
+                        onChange={this.updateEmailField}
+                    />
+                    <input
+                        type='password'
+                        name='password'
+                        placeholder='Enter your password...'
+                        onChange={this.updateEmailField}
+                    />
+                    <input
+                        type='password'
+                        name='password'
+                        placeholder='Confirm your password...'
+                        onChange={this.updateEmailField}
+                    />
+                    <input
+                        type='text'
+                        name='firstName'
+                        placeholder='Enter your first name...'
+                        onChange={this.updateEmailField}
+                    />
+                    <input
+                        type='text'
+                        name='lastName'
+                        placeholder='Enter your last name...'
+                        onChange={this.updateEmailField}
+                    />
+                    <input type="submit" value="Submit"/>
+                </form>
             </div>
         );
     }
