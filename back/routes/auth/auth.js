81d7a7806ef664eed91112e000a0cb4c4b503e4b
Sofia <sofiaestanqueirorocha@gmail.com>
2019-06-24 14:53:39 +0100

    040 quest

--- back/routes/auth/auth.js
+++ back/routes/auth/auth.js
@@ -1,8 +1,25 @@
 const express = require('express');
 const router = express.Router();
+const connection = require('../../helpers/db');
 
 router.post('/signup', function(req, res, next) {
-    res.send('I am in POST signup');
+    const user  = {
+        email: req.body.email,
+        password: req.body.password,
+        name: req.body.name,
+        lastname: req.body.lastname
+    };
+    const query = connection.query(
+        'INSERT INTO users (email, password, name, lastname) VALUES (?)',
+        user,
+        function (error, results, fields) {
+            if(error) {
+                console.log("error", error);
+                res.status(500).end();
+            }
+            res.end();
+        }
+    );
 });
 
 module.exports = router;
