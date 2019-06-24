f2e305409d9b7ca26322c3bf4b1342c740e55f74
Sofia <sofiaestanqueirorocha@gmail.com>
2019-06-24 15:42:23 +0100

    fix bug with insert user at the database

--- back/routes/auth/auth.js
+++ back/routes/auth/auth.js
@@ -3,14 +3,15 @@ const router = express.Router();
 const connection = require('../../helpers/db');
 
 router.post('/signup', function(req, res, next) {
-    const user  = {
-        email: req.body.email,
-        password: req.body.password,
-        name: req.body.name,
-        lastname: req.body.lastname
-    };
+    const user  = [
+        req.body.email,
+        req.body.password,
+        req.body.name,
+        req.body.lastname
+    ];
+
     const query = connection.query(
-        'INSERT INTO users (email, password, name, lastname) VALUES (?)',
+        'INSERT INTO users (email, password, name, lastname) VALUES (?,?,?,?)',
         user,
         function (error, results, fields) {
             if(error) {
