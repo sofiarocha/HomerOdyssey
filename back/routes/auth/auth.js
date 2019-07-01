5968c078397d9656a8697a1198bd8205ad5ab437
Sofia <sofiaestanqueirorocha@gmail.com>
2019-07-01 12:12:34 +0100

    050-quest

--- back/routes/auth/auth.js
+++ back/routes/auth/auth.js
@@ -7,16 +7,18 @@ router.post('/signup', function(req, res, next) {
         req.body.email,
         req.body.password,
         req.body.name,
-        req.body.lastname
+        req.body.lastName
     ];
 
     const query = connection.query(
         'INSERT INTO users (email, password, name, lastname) VALUES (?,?,?,?)',
         user,
         function (error, results, fields) {
-            if(error) {
-                console.log("error", error);
-                res.status(500).end();
+            if (error) {
+                res.status(500).json({ flash:  error.message });
+            }
+            else {
+                res.status(200).json({ flash:  "User has been signed up!" });
             }
             res.end();
         }
